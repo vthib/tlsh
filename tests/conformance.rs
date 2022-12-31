6998be3b45c8d7de37eb477672b6e090dc722b0d
Vincent Thiberville <vthib@pm.me>
2022-12-31 18:28:40 +0100

    test: use exp files from tlsh in conformance tests

--- tests/conformance.rs
+++ tests/conformance.rs
@@ -1,42 +1,27 @@
-use std::collections::HashMap;
 use std::io::BufRead;
-use std::path::Path;
 
 fn test<F>(path: &str, compute_hash: F)
 where
     F: Fn(&[u8]) -> String,
 {
-    // Build conformance results map
     let f = std::fs::File::open(path).unwrap();
     let reader = std::io::BufReader::new(f);
-    let expected_results = reader
-        .lines()
-        .map(|line| {
-            let line = line.unwrap();
-            let mut line = line.split('\t');
-            let expected_hash = line.next().unwrap();
-            let filepath = line.next().unwrap();
+    for line in reader.lines() {
+        let line = line.unwrap();
+        dbg!(&line);
+        let mut line = line.split('\t');
+        let expected_hash = line.next().unwrap();
+        let filepath = line.next().unwrap();
 
-            (filepath.to_owned(), expected_hash.to_owned())
-        })
-        .collect::<HashMap<_, _>>();
-
-    for file in glob::glob("tests/assets/tlsh/example_data/*").unwrap() {
-        let file = file.unwrap();
-        let contents =
-            std::fs::read(&file).unwrap_or_else(|e| panic!("cannot read file {:?}: {}", file, e));
-        let expected_hash = expected_results
-            .get(&format!(
-                "example_data/{}",
-                Path::new(file.file_name().unwrap()).display()
-            ))
-            .unwrap();
+        let fullpath = format!("tests/assets/tlsh/{}", filepath);
+        let contents = std::fs::read(&fullpath)
+            .unwrap_or_else(|e| panic!("cannot read file {:?}: {}", fullpath, e));
 
         assert_eq!(&compute_hash(&contents), expected_hash);
     }
 }
 
-macro_rules! do_test {
+macro_rules! do_hash_test {
     ($path:expr, $type:ty) => {{
         test($path, |contents| {
             let mut tlsh = <$type>::new();
@@ -47,26 +32,41 @@ macro_rules! do_test {
 }
 
 #[test]
-fn test_conformance_file_48_1() {
-    do_test!("tests/assets/example_data.out.48.1", tlsh2::Tlsh48_1);
+fn test_hash_48_1() {
+    do_hash_test!(
+        "tests/assets/tlsh/exp/example_data.48.1.len.out_EXP",
+        tlsh2::Tlsh48_1
+    );
 }
 
 #[test]
-fn test_conformance_file_128_1() {
-    do_test!("tests/assets/example_data.out.128.1", tlsh2::Tlsh128_1);
+fn test_hash_128_1() {
+    do_hash_test!(
+        "tests/assets/tlsh/exp/example_data.128.1.len.out_EXP",
+        tlsh2::Tlsh128_1
+    );
 }
 
 #[test]
-fn test_conformance_file_128_3() {
-    do_test!("tests/assets/example_data.out.128.3", tlsh2::Tlsh128_3);
+fn test_hash_128_3() {
+    do_hash_test!(
+        "tests/assets/tlsh/exp/example_data.128.3.len.out_EXP",
+        tlsh2::Tlsh128_3
+    );
 }
 
 #[test]
-fn test_conformance_file_256_1() {
-    do_test!("tests/assets/example_data.out.256.1", tlsh2::Tlsh256_1);
+fn test_hash_256_1() {
+    do_hash_test!(
+        "tests/assets/tlsh/exp/example_data.256.1.len.out_EXP",
+        tlsh2::Tlsh256_1
+    );
 }
 
 #[test]
-fn test_conformance_file_256_3() {
-    do_test!("tests/assets/example_data.out.256.3", tlsh2::Tlsh256_3);
+fn test_hash_256_3() {
+    do_hash_test!(
+        "tests/assets/tlsh/exp/example_data.256.3.len.out_EXP",
+        tlsh2::Tlsh256_3
+    );
 }
